f123e7b7659b11273e75011c252b0297b3ced24c
Nikolai Hrusanov 10a klas <159422252+NikolaiHrusanov@users.noreply.github.com>
2025-07-09 16:40:00 +0300

    Задача: Лабораторията на Проф. Коденщайн
    Помогнах си с изкуствения интелект за по-добри имена на отварите, защото нямах идеи. Не съм използвал ai в кодирането на Win Form. Надявам се задачата ми, да ви хареса.

---
+++ Laboratory_of_profesor_Kondenstain/Laboratory_of_profesor_Kondenstain/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Laboratory_of_profesor_Kondenstain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Btn_Add_Ingreadance = new System.Windows.Forms.Button();
+            this.Btn_boil_a_potion = new System.Windows.Forms.Button();
+            this.Txb_enter_Ingredients = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(251, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Лаборатория за експерименти";
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_result.Location = new System.Drawing.Point(26, 475);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(106, 25);
+            this.lbl_result.TabIndex = 9;
+            this.lbl_result.Text = "резултат:";
+            this.lbl_result.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(31, 161);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 268);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Btn_Add_Ingreadance
+            // 
+            this.Btn_Add_Ingreadance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Add_Ingreadance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Btn_Add_Ingreadance.Location = new System.Drawing.Point(360, 189);
+            this.Btn_Add_Ingreadance.Name = "Btn_Add_Ingreadance";
+            this.Btn_Add_Ingreadance.Size = new System.Drawing.Size(212, 71);
+            this.Btn_Add_Ingreadance.TabIndex = 11;
+            this.Btn_Add_Ingreadance.Text = "Добави съставка";
+            this.Btn_Add_Ingreadance.UseVisualStyleBackColor = true;
+            this.Btn_Add_Ingreadance.Click += new System.EventHandler(this.Btn_Add_Ingreadance_Click);
+            // 
+            // Btn_boil_a_potion
+            // 
+            this.Btn_boil_a_potion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_boil_a_potion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Btn_boil_a_potion.Location = new System.Drawing.Point(360, 327);
+            this.Btn_boil_a_potion.Name = "Btn_boil_a_potion";
+            this.Btn_boil_a_potion.Size = new System.Drawing.Size(212, 71);
+            this.Btn_boil_a_potion.TabIndex = 12;
+            this.Btn_boil_a_potion.Text = "Свари отварата!";
+            this.Btn_boil_a_potion.UseVisualStyleBackColor = true;
+            this.Btn_boil_a_potion.Click += new System.EventHandler(this.Btn_boil_a_potion_Click);
+            // 
+            // Txb_enter_Ingredients
+            // 
+            this.Txb_enter_Ingredients.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Txb_enter_Ingredients.Location = new System.Drawing.Point(31, 102);
+            this.Txb_enter_Ingredients.Name = "Txb_enter_Ingredients";
+            this.Txb_enter_Ingredients.Size = new System.Drawing.Size(288, 38);
+            this.Txb_enter_Ingredients.TabIndex = 13;
+            this.Txb_enter_Ingredients.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 536);
+            this.Controls.Add(this.Txb_enter_Ingredients);
+            this.Controls.Add(this.Btn_boil_a_potion);
+            this.Controls.Add(this.Btn_Add_Ingreadance);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Лаборатория на Проф. Коденщаин";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Btn_Add_Ingreadance;
+        private System.Windows.Forms.Button Btn_boil_a_potion;
+        private System.Windows.Forms.TextBox Txb_enter_Ingredients;
+    }
+}
+
---
+++ Laboratory_of_profesor_Kondenstain/Laboratory_of_profesor_Kondenstain/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Laboratory_of_profesor_Kondenstain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(listBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // string a = "";
+        private void Btn_Add_Ingreadance_Click(object sender, EventArgs e)
+        {
+            string text = Txb_enter_Ingredients.Text.Trim();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                listBox1.Items.Add(text.ToLower());
+                Txb_enter_Ingredients.Clear();
+                Txb_enter_Ingredients.Focus();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_boil_a_potion_Click(object sender, EventArgs e)
+        {
+            bool hasCatHair = false;
+            bool hasLiquidNitrogen = false;
+            bool hasStrawberry = false;
+            bool hasSugar = false;
+            bool hasBattery = false;
+            bool hasWater = false;
+            bool hasChili = false;
+            bool hasUnicornTear = false;
+            bool hasSpiderLeg = false;
+            bool hasPumpkin = false;
+            bool hasMoonDust = false;
+            bool hasRottenFlesh = false;
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string ingredient = listBox1.Items[i].ToString();
+
+                if (ingredient == "котешка козина")   hasCatHair = true;
+                if (ingredient == "течен азот")       hasLiquidNitrogen = true;
+                if (ingredient == "ягода")            hasStrawberry = true;
+                if (ingredient == "захар")            hasSugar = true;
+                if (ingredient == "батерия")          hasBattery = true;
+                if (ingredient == "вода")             hasWater = true;
+                if (ingredient == "люта чушка")       hasChili = true;
+                if (ingredient == "сълза от еднорог") hasUnicornTear = true;
+                if (ingredient == "крак от паяк")     hasSpiderLeg = true;
+                if (ingredient == "тиква")            hasPumpkin = true;
+                if (ingredient == "лунен прах")       hasMoonDust = true;
+                if (ingredient == "гнило месо")       hasRottenFlesh = true;
+            }
+
+            string result = "";
+
+            if (hasCatHair && hasLiquidNitrogen) //котешка козина" & течен азот
+                result = "Създаде невидимост за 10 минути!";
+
+            else if (hasStrawberry && hasSugar) //ягода & захар
+                result = "Създаде вкусно сладко!";
+
+            else if (hasBattery && hasWater)   //батерия & вода
+                result = "Създаде експлозивен серум… стой далеч!";
+
+            else if (hasChili && hasUnicornTear)  //люта чушка & сълза от еднорог
+                result = "Създаде огнен дъх – като дракон!";
+
+            else if (hasSpiderLeg && hasPumpkin)   //крак от паяк & тиква
+                result = "Създаде прокълнат фенер… Хелоуин идва!";
+
+            else if (hasMoonDust && hasWater)     //лунен прах & вода
+                result = "Създаде елексир на сънищата – сладки сънища!";
+
+            else if (hasStrawberry && hasChili)    //ягода & люти чушки
+                result = "Създаде сладко с пламък – пикантна наслада!";
+
+            else if (hasRottenFlesh && hasPumpkin) //гнило месо & тиква
+                result = "Създаде зомби тиквеняк – бягай!";
+
+            else    //може би просто си направи супа
+                result = "Получената отвара бълбука, но нищо интересно не се случва.";
+
+            lbl_result.Text = result;
+        }
+
+    }
+}
